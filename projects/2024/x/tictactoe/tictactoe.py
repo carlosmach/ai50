d981f8bf5d3fc9ea0af744a9aaecee28d87245d7
Carlos M <carlos@marquez.io>
2024-05-24 17:16:27 -0600

    implementation of methods for game

--- projects/2024/x/tictactoe/tictactoe.py
+++ projects/2024/x/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,46 +23,146 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+
+    count_x = 0
+    count_o = 0
+
+    if board == initial_state():
+        return X
+    
+    for row in board:
+        count_x += row.count(X)
+        count_o += row.count(O)
+
+    if count_x > count_o:
+        return O
+    else:
+        return X
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+    for x in range(len(board)):
+        row = board[x]
+        for y in range(len(row)):
+            if row[y] == EMPTY:
+                possible_actions.add((x,y))
+        
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if action not in actions(board):
+        raise Exception("Invalid action")
+
+    new_board = copy.deepcopy(board)    
+    current_player = player(board)
+    row, column = action
+    new_board[row][column] = current_player
+
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+    
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+    
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board):
+        return True
+    
+    if all(cell != EMPTY for row in board for cell in row):
+        return True
+
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    win = winner(board)
+    if win == X: return 1
+    if win == O: return -1
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    
+    current_player = player(board)
+    best_move = None
+
+    if current_player == X:
+        best_value = float('-inf')
+        best_move = None
+        for action in actions(board):
+            value = min_value(result(board, action))
+            if value > best_value:
+                best_value = value
+                best_move = action
+    else:
+        best_value = float('inf')
+        best_move = None
+        for action in actions(board):
+            value = max_value(result(board, action))
+            if value < best_value:
+                best_value = value
+                best_move = action
+
+        
+    return best_move
+
+def max_value(board):
+    if terminal(board):
+        return utility(board)
+    v = float('-inf')
+    for action in actions(board):
+        v = max(v, min_value(result(board, action)))
+    return v
+
+def min_value(board):
+    if terminal(board):
+        return utility(board)
+    v = float('inf')
+    for action in actions(board):
+        v = min(v, max_value(result(board, action)))
+    return v
+
+
+print(player([[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY],[EMPTY, EMPTY, EMPTY]]))
+print(player([[EMPTY, X, EMPTY], [EMPTY, EMPTY, EMPTY],[EMPTY, EMPTY, EMPTY]]))
+print(player([[EMPTY, X, EMPTY], [EMPTY, O, EMPTY],[EMPTY, EMPTY, EMPTY]]))
+print(player([[X, X, EMPTY], [EMPTY, O, EMPTY],[EMPTY, EMPTY, EMPTY]]))
+print(player([[X, X, O], [EMPTY, O, EMPTY],[EMPTY, EMPTY, EMPTY]]))
+print(player([[X, X, O], [X, O, EMPTY],[EMPTY, EMPTY, EMPTY]]))
+print(player([[X, X, O], [X, O, EMPTY],[O, EMPTY, EMPTY]]))
+print("Testing actions...")
+print(actions([[X, X, O], [X, O, EMPTY],[O, EMPTY, EMPTY]]))
